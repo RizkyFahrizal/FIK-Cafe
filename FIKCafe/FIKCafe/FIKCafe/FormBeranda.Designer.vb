98a5c8b635326eb63f91aac7960875511d159365
Rizal <m.rizkyfahrizal1410@gmail.com>
2023-11-16 15:18:49 +0700

    update beranda

---
+++ FIKCafe/FIKCafe/FIKCafe/FormBeranda.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormBeranda
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormBeranda))
+        Me.Ribbon1 = New System.Windows.Forms.Ribbon()
+        Me.master = New System.Windows.Forms.RibbonTab()
+        Me.RibbonPanel1 = New System.Windows.Forms.RibbonPanel()
+        Me.RibbonTab1 = New System.Windows.Forms.RibbonTab()
+        Me.RibbonTab2 = New System.Windows.Forms.RibbonTab()
+        Me.RibbonPanel2 = New System.Windows.Forms.RibbonPanel()
+        Me.RibbonPanel3 = New System.Windows.Forms.RibbonPanel()
+        Me.RibbonPanel4 = New System.Windows.Forms.RibbonPanel()
+        Me.RibbonButton1 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton2 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton3 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton4 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton5 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton6 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton7 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton8 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton9 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton10 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton11 = New System.Windows.Forms.RibbonButton()
+        Me.RibbonButton12 = New System.Windows.Forms.RibbonButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Ribbon1
+        '
+        Me.Ribbon1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Ribbon1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Ribbon1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Ribbon1.Location = New System.Drawing.Point(0, 0)
+        Me.Ribbon1.Minimized = False
+        Me.Ribbon1.Name = "Ribbon1"
+        '
+        '
+        '
+        Me.Ribbon1.OrbDropDown.BorderRoundness = 8
+        Me.Ribbon1.OrbDropDown.Location = New System.Drawing.Point(0, 0)
+        Me.Ribbon1.OrbDropDown.Name = ""
+        Me.Ribbon1.OrbDropDown.Size = New System.Drawing.Size(527, 72)
+        Me.Ribbon1.OrbDropDown.TabIndex = 0
+        Me.Ribbon1.OrbStyle = System.Windows.Forms.RibbonOrbStyle.Office_2013
+        Me.Ribbon1.RibbonTabFont = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Ribbon1.Size = New System.Drawing.Size(979, 148)
+        Me.Ribbon1.TabIndex = 0
+        Me.Ribbon1.Tabs.Add(Me.master)
+        Me.Ribbon1.Tabs.Add(Me.RibbonTab1)
+        Me.Ribbon1.Tabs.Add(Me.RibbonTab2)
+        Me.Ribbon1.TabSpacing = 4
+        Me.Ribbon1.Text = "Ribbon1"
+        '
+        'master
+        '
+        Me.master.Name = "master"
+        Me.master.Panels.Add(Me.RibbonPanel1)
+        Me.master.Text = "Master"
+        '
+        'RibbonPanel1
+        '
+        Me.RibbonPanel1.Items.Add(Me.RibbonButton1)
+        Me.RibbonPanel1.Items.Add(Me.RibbonButton2)
+        Me.RibbonPanel1.Items.Add(Me.RibbonButton3)
+        Me.RibbonPanel1.Items.Add(Me.RibbonButton4)
+        Me.RibbonPanel1.Name = "RibbonPanel1"
+        Me.RibbonPanel1.Text = "Data Master"
+        '
+        'RibbonTab1
+        '
+        Me.RibbonTab1.Name = "RibbonTab1"
+        Me.RibbonTab1.Panels.Add(Me.RibbonPanel2)
+        Me.RibbonTab1.Text = "Tansaksi"
+        '
+        'RibbonTab2
+        '
+        Me.RibbonTab2.Name = "RibbonTab2"
+        Me.RibbonTab2.Panels.Add(Me.RibbonPanel3)
+        Me.RibbonTab2.Panels.Add(Me.RibbonPanel4)
+        Me.RibbonTab2.Text = "Laporan"
+        '
+        'RibbonPanel2
+        '
+        Me.RibbonPanel2.Items.Add(Me.RibbonButton5)
+        Me.RibbonPanel2.Items.Add(Me.RibbonButton6)
+        Me.RibbonPanel2.Name = "RibbonPanel2"
+        Me.RibbonPanel2.Text = "Transaksi"
+        '
+        'RibbonPanel3
+        '
+        Me.RibbonPanel3.Items.Add(Me.RibbonButton7)
+        Me.RibbonPanel3.Items.Add(Me.RibbonButton8)
+        Me.RibbonPanel3.Items.Add(Me.RibbonButton9)
+        Me.RibbonPanel3.Items.Add(Me.RibbonButton10)
+        Me.RibbonPanel3.Name = "RibbonPanel3"
+        Me.RibbonPanel3.Text = "Master"
+        '
+        'RibbonPanel4
+        '
+        Me.RibbonPanel4.Items.Add(Me.RibbonButton11)
+        Me.RibbonPanel4.Items.Add(Me.RibbonButton12)
+        Me.RibbonPanel4.Name = "RibbonPanel4"
+        Me.RibbonPanel4.Text = "Transaksi"
+        '
+        'RibbonButton1
+        '
+        Me.RibbonButton1.Image = CType(resources.GetObject("RibbonButton1.Image"), System.Drawing.Image)
+        Me.RibbonButton1.LargeImage = CType(resources.GetObject("RibbonButton1.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton1.Name = "RibbonButton1"
+        Me.RibbonButton1.SmallImage = CType(resources.GetObject("RibbonButton1.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton1.Text = "Admin"
+        '
+        'RibbonButton2
+        '
+        Me.RibbonButton2.Image = CType(resources.GetObject("RibbonButton2.Image"), System.Drawing.Image)
+        Me.RibbonButton2.LargeImage = CType(resources.GetObject("RibbonButton2.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton2.Name = "RibbonButton2"
+        Me.RibbonButton2.SmallImage = CType(resources.GetObject("RibbonButton2.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton2.Text = "Pembeli"
+        '
+        'RibbonButton3
+        '
+        Me.RibbonButton3.Image = CType(resources.GetObject("RibbonButton3.Image"), System.Drawing.Image)
+        Me.RibbonButton3.LargeImage = CType(resources.GetObject("RibbonButton3.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton3.Name = "RibbonButton3"
+        Me.RibbonButton3.SmallImage = CType(resources.GetObject("RibbonButton3.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton3.Text = "Kategori Produk"
+        '
+        'RibbonButton4
+        '
+        Me.RibbonButton4.Image = CType(resources.GetObject("RibbonButton4.Image"), System.Drawing.Image)
+        Me.RibbonButton4.LargeImage = CType(resources.GetObject("RibbonButton4.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton4.Name = "RibbonButton4"
+        Me.RibbonButton4.SmallImage = CType(resources.GetObject("RibbonButton4.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton4.Text = "Produk"
+        '
+        'RibbonButton5
+        '
+        Me.RibbonButton5.Image = CType(resources.GetObject("RibbonButton5.Image"), System.Drawing.Image)
+        Me.RibbonButton5.LargeImage = CType(resources.GetObject("RibbonButton5.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton5.Name = "RibbonButton5"
+        Me.RibbonButton5.SmallImage = CType(resources.GetObject("RibbonButton5.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton5.Text = "Pembelian"
+        '
+        'RibbonButton6
+        '
+        Me.RibbonButton6.Image = CType(resources.GetObject("RibbonButton6.Image"), System.Drawing.Image)
+        Me.RibbonButton6.LargeImage = CType(resources.GetObject("RibbonButton6.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton6.Name = "RibbonButton6"
+        Me.RibbonButton6.SmallImage = CType(resources.GetObject("RibbonButton6.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton6.Text = "Penjualan"
+        '
+        'RibbonButton7
+        '
+        Me.RibbonButton7.Image = CType(resources.GetObject("RibbonButton7.Image"), System.Drawing.Image)
+        Me.RibbonButton7.LargeImage = CType(resources.GetObject("RibbonButton7.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton7.Name = "RibbonButton7"
+        Me.RibbonButton7.SmallImage = CType(resources.GetObject("RibbonButton7.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton7.Text = "Admin"
+        '
+        'RibbonButton8
+        '
+        Me.RibbonButton8.Image = CType(resources.GetObject("RibbonButton8.Image"), System.Drawing.Image)
+        Me.RibbonButton8.LargeImage = CType(resources.GetObject("RibbonButton8.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton8.Name = "RibbonButton8"
+        Me.RibbonButton8.SmallImage = CType(resources.GetObject("RibbonButton8.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton8.Text = "Pembeli"
+        '
+        'RibbonButton9
+        '
+        Me.RibbonButton9.Image = CType(resources.GetObject("RibbonButton9.Image"), System.Drawing.Image)
+        Me.RibbonButton9.LargeImage = CType(resources.GetObject("RibbonButton9.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton9.Name = "RibbonButton9"
+        Me.RibbonButton9.SmallImage = CType(resources.GetObject("RibbonButton9.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton9.Text = "Kategori Produk"
+        '
+        'RibbonButton10
+        '
+        Me.RibbonButton10.Image = CType(resources.GetObject("RibbonButton10.Image"), System.Drawing.Image)
+        Me.RibbonButton10.LargeImage = CType(resources.GetObject("RibbonButton10.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton10.Name = "RibbonButton10"
+        Me.RibbonButton10.SmallImage = CType(resources.GetObject("RibbonButton10.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton10.Text = "Produk"
+        '
+        'RibbonButton11
+        '
+        Me.RibbonButton11.Image = CType(resources.GetObject("RibbonButton11.Image"), System.Drawing.Image)
+        Me.RibbonButton11.LargeImage = CType(resources.GetObject("RibbonButton11.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton11.Name = "RibbonButton11"
+        Me.RibbonButton11.SmallImage = CType(resources.GetObject("RibbonButton11.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton11.Text = "Laporan Pembelian"
+        '
+        'RibbonButton12
+        '
+        Me.RibbonButton12.Image = CType(resources.GetObject("RibbonButton12.Image"), System.Drawing.Image)
+        Me.RibbonButton12.LargeImage = CType(resources.GetObject("RibbonButton12.LargeImage"), System.Drawing.Image)
+        Me.RibbonButton12.Name = "RibbonButton12"
+        Me.RibbonButton12.SmallImage = CType(resources.GetObject("RibbonButton12.SmallImage"), System.Drawing.Image)
+        Me.RibbonButton12.Text = "Laporan Penjualan"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(904, 444)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Logout"
+        '
+        'FormBeranda
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(979, 485)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Ribbon1)
+        Me.KeyPreview = True
+        Me.Name = "FormBeranda"
+        Me.Text = "FormBeranda"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Ribbon1 As Ribbon
+    Friend WithEvents master As RibbonTab
+    Friend WithEvents RibbonPanel1 As RibbonPanel
+    Friend WithEvents RibbonButton1 As RibbonButton
+    Friend WithEvents RibbonButton2 As RibbonButton
+    Friend WithEvents RibbonButton3 As RibbonButton
+    Friend WithEvents RibbonButton4 As RibbonButton
+    Friend WithEvents RibbonTab1 As RibbonTab
+    Friend WithEvents RibbonPanel2 As RibbonPanel
+    Friend WithEvents RibbonButton5 As RibbonButton
+    Friend WithEvents RibbonButton6 As RibbonButton
+    Friend WithEvents RibbonTab2 As RibbonTab
+    Friend WithEvents RibbonPanel3 As RibbonPanel
+    Friend WithEvents RibbonButton7 As RibbonButton
+    Friend WithEvents RibbonButton8 As RibbonButton
+    Friend WithEvents RibbonButton9 As RibbonButton
+    Friend WithEvents RibbonButton10 As RibbonButton
+    Friend WithEvents RibbonPanel4 As RibbonPanel
+    Friend WithEvents RibbonButton11 As RibbonButton
+    Friend WithEvents RibbonButton12 As RibbonButton
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FIKCafe/FIKCafe/FIKCafe/FormBeranda.vb
@@ -0,0 +1,5 @@
+﻿Public Class FormBeranda
+    Private Sub Ribbon1_Click(sender As Object, e As EventArgs) Handles Ribbon1.Click
+
+    End Sub
+End Class
